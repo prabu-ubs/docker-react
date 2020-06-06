939b39d7308652c60f363920e1ffe1c1fbce91f5
Ezhil Raj Panneerselvam <Ezhil@Ezhils-MacBook-Pro.local>
2020-06-06 11:42:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> without docker build.
+          Edit <code>src/App.js</code> without docker build next try.
         </p>
         <a
           className="App-link"
